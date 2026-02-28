eaa2777a67d2fe011678648ce96793608620bca4
bharatlikes <satyamsingoh@gmail.com>
2026-02-28 18:24:51 +0530

    Fix 11ty build and routing

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,33 @@
-
-const { DateTime } = require("luxon");
-const sitemapPlugin = require("@quasibit/eleventy-plugin-sitemap");
-const htmlmin = require("html-minifier-terser");
+const { DateTime } = require('luxon');
+const sitemapPlugin = require('@quasibit/eleventy-plugin-sitemap');
+const htmlmin = require('html-minifier-terser');
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('src/assets');
+  eleventyConfig.addPassthroughCopy('src/admin');
+  eleventyConfig.addPassthroughCopy('robots.txt');
 
-  eleventyConfig.addPassthroughCopy("src/assets");
-  eleventyConfig.addPassthroughCopy("src/admin");
-  eleventyConfig.addPassthroughCopy("robots.txt");
-
-  eleventyConfig.addFilter("postDate", dateObj => {
-    return DateTime.fromJSDate(dateObj).toFormat("dd LLL yyyy");
+  eleventyConfig.addFilter('postDate', (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat('dd LLL yyyy');
   });
 
-  eleventyConfig.addCollection("blog", collectionApi => {
-    return collectionApi.getFilteredByGlob("content/blog/*.md")
+  eleventyConfig.addCollection('blog', (collectionApi) => {
+    return collectionApi
+      .getFilteredByGlob('content/blog/*.md')
       .sort((a, b) => b.date - a.date);
   });
 
-  eleventyConfig.addTransform("htmlmin", function(content, outputPath) {
-    if (process.env.NODE_ENV === "production" && outputPath && outputPath.endsWith(".html")) {
+  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
+    if (
+      process.env.NODE_ENV === 'production' &&
+      outputPath &&
+      outputPath.endsWith('.html')
+    ) {
       return htmlmin.minify(content, {
         collapseWhitespace: true,
         removeComments: true,
         minifyCSS: true,
-        minifyJS: true
+        minifyJS: true,
       });
     }
     return content;
@@ -32,15 +35,15 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPlugin(sitemapPlugin, {
     sitemap: {
-      hostname: "https://yourdomain.com"
-    }
+      hostname: 'https://yourdomain.com',
+    },
   });
 
   return {
     dir: {
-      input: "src",
-      includes: "_includes",
-      output: "_site"
-    }
+      input: '.',
+      includes: '_includes',
+      output: '_site',
+    },
   };
 };
